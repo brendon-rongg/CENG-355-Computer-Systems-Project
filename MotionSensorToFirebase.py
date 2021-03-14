732f684d0f625225bd60efad7977af051dbbc21a
Bmjr1213 <54966783+Bmjr1213@users.noreply.github.com>
2021-03-14 17:53:59 -0400

    Create MotionSensorToFirebase.py

---
+++ MotionSensorToFirebase.py
@@ -0,0 +1,55 @@
+#from firebase import firebase
+from gpiozero import MotionSensor
+from gpiozero import LED
+import pyrebase
+
+
+config = {
+  "apiKey": "",
+  "authDomain": "cssproject-9fa41-default-rtdb.firebaseapp.com",
+  "databaseURL": "https://cssproject-9fa41-default-rtdb.firebaseio.com/",
+  "storageBucket": "cssproject-9fa41-default-rtdb.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+# Get a reference to the database service
+db = firebase.database()
+
+
+# import RPi.GPIO as GPIO
+# import time
+
+green_led = LED(26)
+red_led = LED(13)
+pir = MotionSensor (11) # the data pin
+red_led.on()
+green_led.off()
+
+#url =  'https://cssproject-9fa41-default-rtdb.firebaseio.com/' #database url
+#firebase = firebase.FirebaseApplication(url)#real-time datbase url
+#db = firebase.database()
+
+while True:
+
+    pir.wait_for_motion() #pir.when_motion()
+    red_led.off()
+    green_led.on()
+    print("Motion Detected")
+    obj_temp = 37
+    #obj_temp = input()
+    #result = firebase.post("/Apple/tyler/temps",obj_temp)
+    
+    
+    data={'date':"2021/02/01",'email':"ty1@gmail.com",'temp':38}
+    db.child("ty Inc").child("temperature").child("-MP5Lk3xLeY4i66Shhhj").set(data)
+    
+    
+    #firebase.post({'date':"2021/02/01",'email':"appletist@gmail.com",'temp':38})
+    #firebase.post(data)
+    print(obj_temp)
+
+
+    pir.wait_for_no_motion() #pir.when_no_motion()
+    green_led.off()
+    red_led.on()
+    print("Motion Stopped")
